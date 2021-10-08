c7e746840288f2a7f21a3390c014b3cca613f739
Renatto Pantoja <renatto879@hotmail.com>
2021-10-08 11:49:02 -0500

    Add background on initial view

--- app/src/main/java/com/example/cookiemaker/adapters/RecipesListAdapter.kt
+++ app/src/main/java/com/example/cookiemaker/adapters/RecipesListAdapter.kt
@@ -9,24 +9,32 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.cookiemaker.R
 import pe.edu.ulima.pm.ulgamestore.model.Receta
 
-class RecipesListAdapter(private val recipesList: List<Receta>) :
+class RecipesListAdapter(
+    private val recipesList: List<Receta>,
+    private val listener: (Int) -> Unit
+) :
     RecyclerView.Adapter<RecipesListAdapter.ViewHolder>() {
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val iviRecipeImage : ImageView
-        val tviUserName : TextView
-        val tviRecipeName :TextView
 
-        init{
-            iviRecipeImage =view.findViewById(R.id.iviRecipeImage)
-            tviUserName =view.findViewById(R.id.tviUserName)
-            tviRecipeName =view.findViewById(R.id.tviRecipeName)
+    class ViewHolder(view: View,val listener : (Int)->Unit) : RecyclerView.ViewHolder(view), View.OnClickListener {
+        val iviRecipeImage: ImageView
+        val tviUserName: TextView
+        val tviRecipeName: TextView
+
+        init {
+            iviRecipeImage = view.findViewById(R.id.iviRecipeImage)
+            tviUserName = view.findViewById(R.id.tviUserName)
+            tviRecipeName = view.findViewById(R.id.tviRecipeName)
+            view.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            listener(adapterPosition)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).
-        inflate(R.layout.item_recipe,parent,false)
-        val viewHolder = ViewHolder(view)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_recipe, parent, false)
+        val viewHolder = ViewHolder(view,listener)
         return viewHolder
     }
 
--- app/src/main/java/com/example/cookiemaker/fragments/RecipesFragment.kt
+++ app/src/main/java/com/example/cookiemaker/fragments/RecipesFragment.kt
@@ -1,6 +1,7 @@
 package com.example.cookiemaker.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cookiemaker.R
 import com.example.cookiemaker.adapters.RecipesListAdapter
+import pe.edu.ulima.pm.ulgamestore.model.Receta
 import pe.edu.ulima.pm.ulgamestore.model.RecetasManager
 
 class RecipesFragment : Fragment() {
@@ -23,6 +25,11 @@ class RecipesFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val rviRecipes = view.findViewById<RecyclerView>(R.id.rviRecipes)
-        rviRecipes.adapter = RecipesListAdapter(RecetasManager().getRecetas())
+        rviRecipes.adapter = RecipesListAdapter(
+            RecetasManager().getRecetas(),
+            {recipe : Int ->
+                //Log.i ( "RecipeFragment",recipe.nombre)
+            }
+        )
     }
 }
